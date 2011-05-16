20155826372079d8f38cbbb0ae0bc1d10a83ee4c
John Napiorkowski <jjn1056@yahoo.com>
2011-05-16 15:25:07 -0400

    fixed incorrect join condition on a relationship which dies on newer and more strict DBIC versions

--- t/lib/DBSchema/Result/Personality.pm
+++ t/lib/DBSchema/Result/Personality.pm
@@ -9,7 +9,7 @@ __PACKAGE__->load_components( "PK::Auto", "Core" );
 __PACKAGE__->table("personality");
 __PACKAGE__->add_columns( "user_id" => { data_type => 'integer' }, );
 __PACKAGE__->set_primary_key("user_id");
-__PACKAGE__->has_one( 'user', 'DBSchema::Result::User', {'user_id' => 'foreign.id'}, );
+__PACKAGE__->has_one( 'user', 'DBSchema::Result::User', {'foreign.id' => 'self.user_id'}, );
 
 1;
 
